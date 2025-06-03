610a45db6c88c6767635435cf3308e8e650f54d5
venky469 <128397211+venky469@users.noreply.github.com>
2025-06-03 22:47:31 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -110,6 +110,7 @@ mongoose
   })
 
 // Routes
+app.use("/health", (req, res) => res.status(200).json({ status: "OK" }))
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/labours", labourRoutes)
